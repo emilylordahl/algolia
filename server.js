5088791260a7c1acff88ad88fe354754253084f3
Emily Lordahl <emilylordahl@Emilys-MBP.home>
2016-11-27 23:26:47 -0500

    adding express and moving some files around for convention

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+// Express
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+
+// Algolia
 var algoliasearch = require('algoliasearch');
 var applicationID = process.env.ALGOLIA_APP_ID;
 var apiKey = process.env.ALGOLIA_API_KEY;
 var client = algoliasearch(applicationID, apiKey);
 var index = client.initIndex('restaurants');
 var restaurantsJSON = require('./resources/dataset/restaurants_list.json');
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+
+app.use(express.static(__dirname + '/public'));
 
 index.addObjects(restaurantsJSON, function(err, content){
   if (err) {
@@ -13,4 +25,9 @@ index.addObjects(restaurantsJSON, function(err, content){
 
 index.search('Pepper Pike', function searchDone(err, content) {
   console.log(content);
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server running on port 3000...');
 });
